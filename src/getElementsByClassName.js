b61a5c4058e5edf9539bf75fbbd04de1cc9bfeb8
nligon <nathanaelligon@gmail.com>
2016-03-21 02:20:25 -0700

    Completed required portions of recursion sprint, stringifyJSON and getElementsByClassName.

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,16 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-){
-  // your code here
+var getElementsByClassName = function(className, obj) {
+    var results = [];
+    if (obj === undefined) {
+        obj = document;
+    };
+    for (var i = 0; i < obj.children.length; i++) {
+        if (obj.children[i].classList.contains(className)) {
+            results.push(obj.children[i]);
+        }
+        results = results.concat(getElementsByClassName(className, obj.children[i]));
+    }
+    return results;
 };
