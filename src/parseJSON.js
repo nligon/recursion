3db7d6af4bd055bc8fd62ab8b5c642e34d44c88e
nligon <nathanaelligon@gmail.com>
2016-07-25 12:31:07 -0700

    Implement initial code for number detection.

--- src/parseJSON.js
+++ src/parseJSON.js
@@ -4,6 +4,12 @@
 //  it has a terminating criteria where if you hit a primitive, you just do something (don't recurse)
 // when it's an Array or Object, you recurse, for each element, run the function, return JSON object or primitive
 // but you're not, so you'll write it from scratch:
+function testNumber(str){
+  if (str.charAt(0) === 0 || 1 || 2 || 3 || 4 || 5 || 6 || 7 || 8 || 9) {
+    console.log("Number found!");
+  }
+}
+
 var parseJSON = function(json) {
   var res;
   // your code goes here
@@ -11,12 +17,18 @@ var parseJSON = function(json) {
   // if object
 
   // if array
+
+  // else, return primitive (base case)
+  // if string
   if (json.charAt(0) === '"') {
     console.log('String detected!');
     res = json.slice(1, json.length - 1);
-  } //if it's a string...
-  // else, return primitive (base case)
+  } else if (json.charAt(0)) {
+
+  }
+  
   return res;
 };
 
-console.log(parseJSON('"test"'));
+// console.log(parseJSON('"test"'));
+console.log(testNumber('5'))
